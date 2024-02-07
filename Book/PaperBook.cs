4f8e1ac8c292e95bb5a4a90bb558a7c9047a91e3
ACER <ACER@LAPTOP-PLKL2729>
2024-02-07 17:40:58 +0700

    Add project files.

---
+++ Book/PaperBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Book
+{
+    public class PaperBook : Book
+    {
+        public PaperBook(int id, string name, string description, string author, DateTime publishDate, string translator) : base(id, name, description, author, publishDate, translator)
+        {
+        }
+    }
+}
---
+++ Library/ILibrary.cs
@@ -0,0 +1,15 @@
+﻿using Library.Book;
+
+namespace Library.Library
+{
+    public interface ILibrary
+    {
+        // defined name method
+        // Rule: verb + subject
+        List<Book.Book> GetBooks();
+        Book.Book? GetBook(int id);
+        void AddBook(Book.Book book);
+        void DeleteBook(int id);
+        void UpdateBook(Book.Book book);
+    }
+}
